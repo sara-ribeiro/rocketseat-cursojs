69c99ea4db6d60212af57f0dbac742197330ad7a
unknown <sara_r_machado@saojose.sc.senai.br>
2019-06-17 12:37:25 -0300

    Aula 3 modulo 3

---
+++ mod3/aula/2/todo.js
@@ -0,0 +1,13 @@
+var listElement = 
+document.querySelector("#app ul");
+var inputElement =
+document.querySelector("#app input");
+var buttonElement =
+document.querySelector("#app button");
+
+var todos = [
+    'arrumar queixo',
+    ''
+]
+console.log(listElement)
+console.log(inputElement)
